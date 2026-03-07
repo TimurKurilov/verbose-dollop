1d29f3ccae3867c4e73897551091a37262acb165
TimurKurilov <kyrilov.timur@gmail.com>
2026-03-07 21:16:33 +0300

    task limit

--- verbose/content/models.py
+++ verbose/content/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
-    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(blank=False, max_length=55)
     desc = models.TextField(blank=True, max_length=255)
     date = models.DateTimeField(default=timezone.now)
--- verbose/content/views.py
+++ verbose/content/views.py
@@ -9,14 +9,18 @@ def page(request):
     return render(request, "content/page.html")
 
 def create_task(request):
-    tasks_count = Task.objects.all(owner=request.user).count()
-    if tasks_count > 3:
+    if not request.user.is_authenticated:
+            return redirect("login")
+    tasks_count = Task.objects.filter(user=request.user).count()
+    if tasks_count >= 3:
         messages.error(request, f"На сегодня все братик")
         return redirect("all_task")
 
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
             form.save()
             return HttpResponseRedirect("/list/")
     else:
@@ -24,7 +28,7 @@ def create_task(request):
     return render(request, "content/create_task.html", {"form": form})
 
 def all_task(request):
-    tasks = Task.objects.all(owner=request.user)
+    tasks = Task.objects.filter(user=request.user)
     if not request.user.is_authenticated:
         return redirect("register")
     return render(request, "content/all_tasks.html", {"tasks": tasks})
