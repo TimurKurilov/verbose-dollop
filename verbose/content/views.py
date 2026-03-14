4dd070e673745d85f4d0592f6d9f0cb97259fcc7
TimurKurilov <kyrilov.timur@gmail.com>
2026-03-14 21:38:23 +0300

    fixes

--- verbose/content/views.py
+++ verbose/content/views.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-rom django.shortcuts import redirect, render
+from django.shortcuts import redirect, render
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from content.forms import TaskForm
@@ -11,12 +11,12 @@ def page(request):
 
 def create_task(request):
     if not request.user.is_authenticated:
-            return redirect("login")
+        return redirect("login")
     tasks_count_by_date = Task.objects.filter(
-        user=request.user
-        date__date = timezone.now().date()
+        user=request.user,
+        date__date = timezone.now().date(),
         ).count()
-    if tasks_count >= 3:
+    if tasks_count_by_date >= 3:
         messages.error(request, f"На сегодня все братик")
         return redirect("all_task")
 
@@ -25,14 +25,15 @@ def create_task(request):
         if form.is_valid():
             task = form.save(commit=False)
             task.user = request.user
-            form.save()
-            return HttpResponseRedirect("/list/")
+            task.date = timezone.now()
+            task.save()
+            return HttpResponseRedirect("all_task")
     else:
         form = TaskForm()
     return render(request, "content/create_task.html", {"form": form})
 
 def all_task(request):
-    tasks = Task.objects.filter(user=request.user)
     if not request.user.is_authenticated:
         return redirect("register")
+    tasks = Task.objects.filter(user=request.user)
     return render(request, "content/all_tasks.html", {"tasks": tasks})
