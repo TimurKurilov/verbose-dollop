7345fd749510301e19a87b65b324b5a9185b3bd8
TimurKurilov <kyrilov.timur@gmail.com>
2026-03-30 12:10:24 +0300

    upd model

--- verbose/content/models.py
+++ verbose/content/models.py
@@ -1,4 +1,3 @@
-# models.py
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
@@ -7,13 +6,14 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=55)
-    slot = models.IntegerField(default=0)
+    slot = models.IntegerField()
+    day = models.DateField()
     date = models.DateTimeField(default=timezone.now)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["user", "date__date", "slot"],
+                fields=["user", "day", "slot"],
                 name="unique_user_day_slot"
             )
         ]
