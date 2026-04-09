d8b8ffb8402d010630911d6bd256accbfaa2fa9d
TimurKurilov <kyrilov.timur@gmail.com>
2026-04-09 21:09:34 +0300

    del task

--- verbose/content/views.py
+++ verbose/content/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from django.utils import timezone
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from content.forms import TaskForm
@@ -54,7 +54,7 @@ def create_task(request):
 
 def all_task(request):
     if not request.user.is_authenticated:
-        return redirect("register")
+        return redirect("login")
     tasks = Task.objects.filter(user=request.user)
     return render(request, "content/all_tasks.html", {"tasks": tasks})
 
@@ -62,3 +62,11 @@ def tasks_by_date(request, date):
     parsed_date = datetime.strptime(date, "%Y-%m-%d").date()
     tasks_by_date = Task.objects.filter(user=request.user, day=parsed_date)
     return render(request, "content/tasks_by_date.html", {"tasks_by_date": tasks_by_date})
+
+def task_delete(request, date):
+    parsed_date = datetime.strptime(date, "%Y-%m-%d").date()
+    if not request.user.is_authenticated:
+        return redirect("login")
+    if task.user == request.user:
+        task = get_object_or_404(Task, created=date)
+        return redirect("all_task")
